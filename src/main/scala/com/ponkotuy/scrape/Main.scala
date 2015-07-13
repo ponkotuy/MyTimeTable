6a94b9535e71523192f81d912e9e71d30e0c410f
ponkotuy <web@ponkotuy.com>
2015-07-14 00:55:39 +0900

    Add parser to a train table

--- src/main/scala/com/ponkotuy/scrape/Main.scala
+++ src/main/scala/com/ponkotuy/scrape/Main.scala
@@ -1,24 +1,35 @@
 package com.ponkotuy.scrape
 
-import com.ponkotuy.scrape.model.TimeTablePage
+import com.ponkotuy.scrape.model.{TimeTablePage, TrainPage}
+
+import scala.xml.NodeSeq
 
 object Main {
-  // wget & XMLParse
+  implicit val codec = scalax.io.Codec("Shift_JIS")
+  // load & XMLParse
   def scrape(url: String): TimeTablePage = {
-    import scala.xml._
-    import java.net.URL
-    import scalax.io.JavaConverters._
-    import net.liftweb.util.Html5
-
-    implicit val codec = scalax.io.Codec("Shift_JIS")
-
-    val str = new URL(url).asInput.string
-    val xml = Html5.parse(str) openOr NodeSeq.Empty
+    val xml = loadXML(url)
     TimeTablePage.fromXML(xml)
   }
 
   def main(args: Array[String]) {
-    val table = scrape("http://www.ekikara.jp/newdata/line/2301011/down1_1.htm")
-    println(table.trains.map(_.url))
+    // val table = scrape("http://www.ekikara.jp/newdata/line/2301011/down1_1.htm")
+    // println(table.trains.map(_.url))
+    val host = "http://www.ekikara.jp"
+    val url = "/newdata/detail/2301011/36158.htm"
+    val train = TrainPage.fromXML(loadXML(s"${host}${url}"), url)
+    println(train)
+  }
+
+  def loadXML(url: String): NodeSeq = {
+    import java.net.URL
+
+    import net.liftweb.util.Html5
+
+    import scala.xml._
+    import scalax.io.JavaConverters._
+
+    val str = new URL(url).asInput.string
+    Html5.parse(str) openOr NodeSeq.Empty
   }
 }
